fc3640d561154d9ffed508a695d5b7159dd50cfe
Brendan Long <self@brendanlong.com>
2015-04-10 13:23:19 -0500

    Switch to requirements.txt for requirements.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 virtualenv -p python3 .
 source bin/activate
-pip install --upgrade bitstring crcmod
+pip install -r requirements.txt
 
 echo "You probably want to 'source bin/activate' now."
